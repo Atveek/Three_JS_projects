b4b2a850c8b8ad76c780f48abab9aaefbb99b94b
Atveek <atveekdungarani2021@gmail.com>
2024-11-27 10:41:21 +0530

    Solar System Project is complet

--- Solar System/src/js/script.js
+++ Solar System/src/js/script.js
@@ -35,7 +35,7 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 camera.position.set(-90, 140, 140);
 orbit.update();
 
-const ambientLight = new THREE.AmbientLight(0x333333);
+const ambientLight = new THREE.AmbientLight(0x55555);
 scene.add(ambientLight);
 
 // Load cube background
@@ -58,6 +58,58 @@ const sunMtl = new THREE.MeshBasicMaterial({
 const sun = new THREE.Mesh(sunGeo, sunMtl);
 scene.add(sun);
 
+//pointLight - color of light , intensity , max area of light
+const pointLight = new THREE.PointLight(0xffffff, 8000, 300);
+scene.add(pointLight);
+
+//create Planet and Ring
+function createPlanet(size, texture, position, ring) {
+  const geo = new THREE.SphereGeometry(size, 30, 30);
+  const mtl = new THREE.MeshStandardMaterial({
+    map: textureLoader.load(texture),
+  });
+  const mesh = new THREE.Mesh(geo, mtl);
+  const obj = new THREE.Object3D();
+  obj.add(mesh);
+
+  if (ring) {
+    const ringGeo = new THREE.RingGeometry(
+      ring.innerRadius,
+      ring.outerRadius,
+      30
+    );
+    const ringMtl = new THREE.MeshBasicMaterial({
+      map: textureLoader.load(ring.texture),
+      side: THREE.DoubleSide,
+    });
+    const ringO = new THREE.Mesh(ringGeo, ringMtl);
+    obj.add(ringO);
+    ringO.position.x = position;
+    ringO.rotation.x = -0.5 * Math.PI;
+  }
+  scene.add(obj);
+  mesh.position.x = position;
+  return { mesh, obj };
+}
+
+const mercury = createPlanet(3.2, mercuryTexture, 28);
+const venus = createPlanet(5.8, venusTexture, 44);
+const earth = createPlanet(6, earthTexture, 62);
+const mars = createPlanet(4, marsTexture, 78);
+const jupiter = createPlanet(12, jupiterTexture, 100);
+const saturn = createPlanet(10, saturnTexture, 138, {
+  innerRadius: 10,
+  outerRadius: 20,
+  texture: saturnRingTexture,
+});
+const uranus = createPlanet(7, uranusTexture, 176, {
+  innerRadius: 7,
+  outerRadius: 12,
+  texture: uranusRingTexture,
+});
+const neptune = createPlanet(7, neptuneTexture, 200);
+const pluto = createPlanet(2.8, plutoTexture, 216);
+
 // Handle window resize
 window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -67,7 +119,35 @@ window.addEventListener("resize", function () {
 
 // Animation loop
 function animate() {
-  renderer.render(scene, camera);
+  // Sun
+  sun.rotateY(0.004); // Sun's rotation
+
+  // Self-Rotation (Spin)
+  mercury.mesh.rotateY(0.004); // Counterclockwise
+  venus.mesh.rotateY(-0.002); // Clockwise (Retrograde)
+  earth.mesh.rotateY(0.02); // Counterclockwise
+  mars.mesh.rotateY(0.018); // Counterclockwise
+  jupiter.mesh.rotateY(0.04); // Counterclockwise
+  saturn.mesh.rotateY(0.038); // Counterclockwise
+  uranus.mesh.rotateY(-0.03); // Clockwise (Retrograde)
+  neptune.mesh.rotateY(0.032); // Counterclockwise
+  pluto.mesh.rotateY(0.008); // Counterclockwise
+
+  // Orbital Rotation (Around the Sun)
+  mercury.obj.rotateY(0.04); // Counterclockwise
+  venus.obj.rotateY(0.015); // Counterclockwise
+  earth.obj.rotateY(0.01); // Counterclockwise
+  mars.obj.rotateY(0.008); // Counterclockwise
+  jupiter.obj.rotateY(0.002); // Counterclockwise
+  saturn.obj.rotateY(0.0009); // Counterclockwise
+  uranus.obj.rotateY(0.0004); // Counterclockwise
+  neptune.obj.rotateY(0.0001); // Counterclockwise
+  pluto.obj.rotateY(0.00007); // Counterclockwise
+
+  renderer.render(scene, camera); // Render the scene with the camera
 }
+
+renderer.setAnimationLoop(animate);
+
 
 renderer.setAnimationLoop(animate);
